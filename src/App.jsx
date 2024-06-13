85f99f18bbf7e9908c1b347449295505cd03c171
Mrcoder <amirsohail4151@gmail.com>
2024-06-13 08:03:17 -0700

    Toggle Style

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+
+  return (
+
+    <input
+
+      placeholder="Enter your text..."
+      class="input"
+      name="text"
+      type="text">
+
+    </input>
+
+  )
+}
+
+export default App
